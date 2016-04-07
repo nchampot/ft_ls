db55563c78b9d2aad99e389b82f53c8b4960b3e6
Pouyan GHASSEMI <pghassem@e2r3p7.42.fr>
2016-04-07 19:19:59 +0200

    norme

--- parse_args.c
+++ parse_args.c
@@ -1,38 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pghassem <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/07 18:58:16 by pghassem          #+#    #+#             */
+/*   Updated: 2016/04/07 19:19:34 by pghassem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
 static int	is_opt(char c)
 {
-	return ((c == 'a') || (c =='R') || (c == 'l') || (c == 't') || (c == 'r'));
+	return ((c == 'a') || (c == 'R') || (c == 'l') || (c == 't') || (c == 'r'));
 }
 
-int	parse_args(int ac, char **av, char ***startdirs, char **opts)
+int			parse_args(int ac, char **av, char ***startdirs, char **opts)
 {
 	int	i;
-	int	j;
 
 	i = 1;
 	while (av[i])
 	{
 		if (**startdirs == '\0' && av[i][0] == '-')
-		{
-			j = 1;
-			while (av[i][j])
-			{
-				if (is_opt(av[i][j]))
-				{
-					if (!ft_strchr(*opts, av[i][j]))
-						ft_addchr(opts, av[i][j]);
-				}
-				else
-					return ((int)av[i][j]);
-				j++;
-			} 
-		}
+			parse(i, 1, av, opts);
 		else if (av[i][0] != '-')
 			ft_addstr(startdirs, av[i]);
 		i++;
 	}
 	if (**startdirs == '\0')
-			ft_addstr(startdirs, ".");
+		ft_addstr(startdirs, ".");
+	return (1);
+}
+
+int			parse(int i, int j, char **av, char **opts)
+{
+	while (av[i][j])
+	{
+		if (is_opt(av[i][j]))
+		{
+			if (!ft_strchr(*opts, av[i][j]))
+				ft_addchr(opts, av[i][j]);
+		}
+		else
+			return ((int)av[i][j]);
+		j++;
+	}
 	return (1);
 }
