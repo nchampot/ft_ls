af0f2df5b96de32f1377523bae8e254eca111884
Noe Champot <nchampot@Noes-Mac.local>
2016-04-21 00:44:18 +0200

    ok

--- sort.c
+++ sort.c
@@ -12,75 +12,79 @@
 
 #include "ft_ls.h"
 
-static void	add_dots(char **to_print, char ***tab)
+static void	add_dots(char **paths, char ***tab)
 {
 	int	i;
 
 	i = 0;
-	while (to_print[i])
+	while (paths[i])
 	{
-		if ((ft_strrchr(to_print[i], '/'))[1] == '.')
-			ft_addstr(tab, to_print[i]);
+		if ((ft_strrchr(paths[i], '/'))[1] == '.')
+			ft_addstr(tab, paths[i]);
 		i++;
 	}
 }
 
-static void	add_uppercases(char **to_print, char ***tab)
+static void	add_uppercases(char **paths, char ***tab)
 {
 	int	i;
 
 	i = 0;
-	while (to_print[i])
+	while (paths[i])
 	{
-		if (ft_isupper((ft_strrchr(to_print[i], '/'))[1])
-				&& (ft_strrchr(to_print[i], '/'))[1] != '.')
-			ft_addstr(tab, to_print[i]);
+		if (ft_isupper((ft_strrchr(paths[i], '/'))[1])
+				&& (ft_strrchr(paths[i], '/'))[1] != '.')
+			ft_addstr(tab, paths[i]);
 		i++;
 	}
 }
 
-static void	add_rest(char **to_print, char ***tab)
+static void	add_rest(char **paths, char ***tab)
 {
 	int	i;
 
 	i = 0;
-	while (to_print[i])
+	while (paths[i])
 	{
-		if (!ft_isupper((ft_strrchr(to_print[i], '/'))[1])
-				&& (ft_strrchr(to_print[i], '/'))[1] != '.')
-			ft_addstr(tab, to_print[i]);
+		if (!ft_isupper((ft_strrchr(paths[i], '/'))[1])
+				&& (ft_strrchr(paths[i], '/'))[1] != '.')
+			ft_addstr(tab, paths[i]);
 		i++;
 	}
 }
 
-char		**lex_sort(char **to_print)
+char		**lex_sort(char **paths)
 {
 	char	**tab;
 	char	**buff;
 
-	if (to_print == NULL)
+	if (paths == NULL)
 		return (NULL);
 	tab = (char**)malloc(sizeof(char*));
 	*tab = NULL;
-	buff = l_sort(to_print);
+	if (paths[0] && ft_strlen(paths[0]) > 3 && ft_strcmp(ft_strsub(paths[0], 0, 4), "/dev") == 0)
+		buff = l_sort(paths);
+	else
+		buff = paths;
+	add_dots(buff, &tab);
 	add_uppercases(buff, &tab);
 	add_rest(buff, &tab);
 	return (tab);
 }
 
-char		**r_sort(char **to_print)
+char		**r_sort(char **paths)
 {
 	int		i;
 	char	**tab;
 
-	if (to_print == NULL)
+	if (paths == NULL)
 		return (NULL);
 	tab = (char**)malloc(sizeof(char*));
 	*tab = NULL;
 	i = 0;
-	while (to_print[i])
+	while (paths[i])
 		i++;
 	while (i--)
-		ft_addstr(&tab, to_print[i]);
+		ft_addstr(&tab, paths[i]);
 	return (tab);
 }
